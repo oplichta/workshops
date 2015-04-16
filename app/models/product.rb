f7c819a1e68d23260d3153167bed86bfb83b89a2
Oskar Plichta <osi22@o2.pl>
2015-04-16 20:04:16 +0200

    Added validators to category, product and review model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :price, :description, :title, presence: true
+  validates :price, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+    reviews.average(:rating)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :user_id, :rating, :content, presence: true
 end
